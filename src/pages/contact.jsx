b01dcd6945a63361b4958953922974b92a425500
289Volts <289volts@gmail.com>
2022-11-04 15:13:51 +0100

    feat:Mobile view 100% done

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,8 +2,6 @@ import Footer from "../components/footer";
 import "../contact.css";
 
 function contact() {
-  
-
 	return (
 		<div className="container">
 			<div className="wrapper">
@@ -11,7 +9,7 @@ function contact() {
 					<h1 className="form-heading">Contact Me</h1>
 					<p className="form-subtitle">Hi there, contact me to ask me about anything you have in mind.</p>
 				</div>
-				<form action="" method="post" onSubmit={handleSubmit(onSubmit)}>
+				<form action="" method="post">
 					<div className="form-input">
 						<label htmlFor="first_name">First Name</label>
 						<input type="text" name="firstName" id="first_name" placeholder="Enter your first name" />
@@ -22,11 +20,20 @@ function contact() {
 					</div>
 					<div className="form-input">
 						<label htmlFor="email">Email</label>
-						<input type="email" name="email" id="email" placeholder="Enter a valid email" />
+						<input type="email" name="email" id="email" placeholder="yourname@email.com" />
 					</div>
 					<div className="form-input">
 						<label htmlFor="message">Your Message</label>
-						<textarea name="message" id="message" rows="8"></textarea>
+						<textarea
+							name="message"
+							id="message"
+							rows="8"
+							placeholder="Send me a message and I'll reply you as soon as possible..."
+						></textarea>
+					</div>
+					<div className="form-input checkbox">
+						<input type="checkbox" name="checkbox" id="agree" />
+						<p htmlFor="agree">You agree to providing your data to {name} who may contact you.</p>
 					</div>
 					<div className="form-cta">
 						<button type="submit">Send Message</button>
